7839391dae721e19cc21594a959ee5245c3ced70
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2024-05-25 15:59:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const myInput = document.getElementById("myInput");
+const mySubmit = document.getElementById("mySubmit");
+const myImage = document.getElementById("myImage");
+
+mySubmit.onclick = async function() {
+
+    try {
+        
+        const name = myInput.value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+        if (!response.ok) {
+            throw new Error("Could not fetch Pokemon");
+        }
+
+        const data = await response.json();
+        const sprite = data.sprites.front_default;
+
+        myImage.src = sprite;
+        myImage.style.display = "block";
+    } catch (error){
+        console.error(error);
+    }
+
+}
+
