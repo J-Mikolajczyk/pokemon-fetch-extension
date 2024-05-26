2bea9c54f41207e1f8b654fcf16e5ff8b1542c5f
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2024-05-26 13:31:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const myInput = document.getElementById("myInput");
 const mySubmit = document.getElementById("mySubmit");
 const myImage = document.getElementById("myImage");
 
-mySubmit.onclick = async function() {
-
+async function fetchPokemon() {
     try {
-        
         const name = myInput.value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-
+        
         if (!response.ok) {
             throw new Error("Could not fetch Pokemon");
         }
-
+        
         const data = await response.json();
         const sprite = data.sprites.front_default;
-
+        
         myImage.src = sprite;
         myImage.style.display = "block";
-    } catch (error){
+    } catch (error) {
         console.error(error);
     }
-
 }
 
+mySubmit.onclick = fetchPokemon;
+
+myInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        fetchPokemon();
+    }
+});
